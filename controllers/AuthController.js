393166180d6a42752ed110c331fbc60be02c6a38
Hoshang Charania <hok14@pitt.edu>
2018-11-17 18:59:41 -0500

    Authentication added/ Session created

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,15 +1,50 @@
+var Users= require('../models/users');
+
 exports.login= async function(req,res){
-res.render('login');
+    if(req.session.user!=null){
+        user=req.session.user;
+        res.redirect('/Users/view/'+user.username);
+    }
+    res.render('login');
 }
 
 exports.authenticate=  async function(req,res){
-    if(!req.body.id || !req.body.password){
+    if(!req.body.username || !req.body.password){
        res.render('login', {message: "Please enter both id and password"});
     } else {
-          if(user.id === req.body.id && user.password === req.body.password){
-             req.session.user = user;
-             res.redirect('/Users/view/'+user);
-          }
-       res.render('error', {message: "Invalid credentials!"});
+        user={};
+        user.username=req.body.username
+        user.password=req.body.password
+        Users.findOne({ 'username': user.username , 'password': user.password}, function(err, user) {
+            if (err) throw err;
+            console.log("here")
+            if(user!=null){
+            req.session.user = user;
+            // show the one user
+            console.log(user);
+            res.redirect('/Users/view/'+user.username);
+            }else{
+                res.render('login', {message: "Invalid credentials!"});
+            }
+          });
+       
+    }
+}
+
+exports.logout=async function(req,res){
+    if(req.session.user!=null){
+        req.session.destroy();
+    }
+    res.render('login');
+}
+
+exports.isAuthenticated= async function (req, res, next){
+    if(req.session.user!=null){
+        next();
+    }
+    else{
+        res.render('login',{
+            message: "Access Denied"
+        });
     }
 }
